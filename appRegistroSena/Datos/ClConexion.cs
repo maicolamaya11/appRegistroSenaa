6e8bc53f6d0a96dc300fc86b6d608fdcd232abaf
Cristian-Salcedo <salcedocristian557@gmail.com>
2023-08-22 10:02:13 -0500

    Agregar archivos de proyecto.

---
+++ appRegistroSena/Datos/ClConexion.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace appRegistroSena.Datos
+{
+    public class ClConexion
+    {
+        SqlConnection conexion = null;
+        public SqlConnection mtdConexion()
+        {
+            conexion = new SqlConnection("");
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
