944d6d875d254ad0ea78ec824f9b4365b8abe672
Cristian-Salcedo <salcedocristian557@gmail.com>
2023-08-22 10:22:11 -0500

    Procesar1

--- appRegistroSena/Datos/ProcesarSQL.cs
+++ appRegistroSena/Datos/ProcesarSQL.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +10,7 @@ namespace appRegistroSena.Datos
 {
     public class ProcesarSQL
     {
-        public SQlCommand mtdPrceso(string proceso)
+        public SqlCommand mtdPrceso(string proceso)
         {
             ClConexion objConexion = new ClConexion();
             SqlCommand comando = new SqlCommand(proceso, objConexion.mtdConexion());
