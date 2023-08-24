7c6863f8a2c4a9af0574bb1ecb0aa528092fd93d
maicolamaya11 <maidax04112004@gmail.com>
2023-08-24 14:16:47 -0500

    RegistroPersonal

---
+++ appRegistroSena/Vista/RegistrarPersonal.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Vista/Administrador.Master" AutoEventWireup="true" CodeBehind="RegistrarPersonal.aspx.cs" Inherits="appRegistroSena.Vista.RegistrarPersonal" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="css/RegistrarPersonal.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div class="registration-container">
+        <div class="registration-image">
+            <img src="Imagenes/senacentro.png" alt="Imagen de Registro">
+        </div>
+        <div class="registration-form">
+            <h2>Registro Personal</h2>
+            <asp:TextBox ID="txtDocumento" runat="server" placeholder="Documento"></asp:TextBox>
+            <asp:TextBox ID="txtNombre" runat="server" placeholder="Nombre"></asp:TextBox>
+            <asp:TextBox ID="txtApellid0" runat="server" placeholder="Apellido"></asp:TextBox>
+            
+             <asp:Label ID="Label1" runat="server" Text="Rol: "></asp:Label>
+                        <select id="ddlRol" name="rol" runat="server">
+                            <option value="Seleccionar Rol">Seleccionar Rol</option>
+                            <option value="Instructor">Instructor</option>
+                            <option value="aprendiz">Aprendiz</option>
+                          
+                        </select>
+            <asp:DropDownList ID="ddlPrograma" runat="server"></asp:DropDownList>
+            <br />
+            <br />
+                <button type="submit">Registrarse</button>
+            
+        </div>
+    </div>
+</asp:Content>
---
+++ appRegistroSena/Vista/RegistrarPersonal.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace appRegistroSena.Vista
+{
+
+
+    public partial class RegistrarPersonal
+    {
+
+        /// <summary>
+        /// Control txtDocumento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDocumento;
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control txtApellid0.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellid0;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control ddlRol.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlSelect ddlRol;
+
+        /// <summary>
+        /// Control ddlPrograma.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlPrograma;
+    }
+}
