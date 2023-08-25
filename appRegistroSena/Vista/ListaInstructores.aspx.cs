d07e6800eab96ef28cf23999ebe85aef8a340539
Diana <malpi@DESKTOP-24921JO>
2023-08-24 23:37:19 -0500

    cambios 2

---
+++ appRegistroSena/Vista/ListaInstructores.aspx.cs
@@ -0,0 +1,23 @@
+﻿using appRegistroSena.Entidades;
+using appRegistroSena.Logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace appRegistroSena.Vista
+{
+    public partial class ListaInstructores : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ClUsuarioL objServicio = new ClUsuarioL();
+            List<ClUsuarioE> lista = objServicio.mtdListarInstructor();
+            gvInstructor.DataSource = lista;
+
+            gvInstructor.DataBind();
+        }
+    }
+}
