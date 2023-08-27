e1db55dcfbb3ba5d32c44284cdc142e85690edd9
najo0518 <majovargas0518@gmail.com>
2023-08-27 13:32:29 -0500

    listar Aprendiz

--- appRegistroSena/Datos/ClConexion.cs
+++ appRegistroSena/Datos/ClConexion.cs
@@ -13,7 +13,7 @@ namespace appRegistroSena.Datos
         public SqlConnection mtdConexion()
         {
 
-            conexion = new SqlConnection("");
+            conexion = new SqlConnection("Data Source=.;Initial Catalog=dbRegistroSena;Integrated Security=True");
 
             conexion.Open();
             return conexion;
